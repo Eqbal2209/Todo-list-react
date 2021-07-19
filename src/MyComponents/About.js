d598e3054060dd9bfb1253256d6297b93e1040ed
Eqbal2209 <62060211+Eqbal2209@users.noreply.github.com>
2021-07-19 08:14:22 +0530

    Create About.js

---
+++ src/MyComponents/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+            this is an about component
+            <p>
+                this todo app is made by <span className="text-light">eqbal khan</span>,<br/>which he made accordance with learning react javascript its his first mini project of reactjs and hope in the future many more coming through
+            </p>
+        </div>
+    )
+}
